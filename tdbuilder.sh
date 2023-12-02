f336a558cd5e545231914ee9285722f0d8115e93
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-12-02 01:53:09 +0000

    feat: Added setup script and configuration files
    Added setup script for macOS and configuration files for text editor

--- tdbuilder.sh
+++ tdbuilder.sh
@@ -78,4 +78,25 @@ if [ $TARGET = "linux" ]; then
         cd ..
         php SplitSource.php --undo
     fi
+    cd ..
+    ls -l td/tdlib
+    exit 0
+elif [ $TARGET = "macos" ]; then
+    xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install gperf cmake openssl
+    git clone https://github.com/tdlib/td.git
+    cd td
+    rm -rf build
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
+    cmake --build . --target install
+    cd ..
+    cd ..
+    ls -l td/tdlib
+    exit 0
 fi
+
+echo "This $TARGET is not available for building."
+exit 1
